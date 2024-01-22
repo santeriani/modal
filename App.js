b44d69c4d08848e4eda6862dda4efd86b0d210a6
Santeri Anias <santeri.anias@gmail.com>
2024-01-22 19:00:09 +0200

    First commit

--- App.js
+++ App.js
@@ -1,20 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Pressable, Modal, StyleSheet } from 'react-native';
 
-export default function App() {
+const App = () => {
+  const [modalVisible, setModalVisible] = useState(false);
+
+  const handlePress = () => {
+    setModalVisible(true);
+  };
+
+  const closeModal = () => {
+    setModalVisible(false);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Pressable onPress={handlePress}>
+        <Text style={styles.text}>Show modal message</Text>
+      </Pressable>
+
+      <Modal
+        visible={modalVisible}
+        onRequestClose={closeModal}
+      >
+        <View style={styles.modalContainer}>
+          <Text style={styles.modalText}>This is modal...</Text>
+          <Pressable onPress={closeModal}>
+            <Text style={styles.closeText}>Close</Text>
+          </Pressable>
+        </View>
+      </Modal>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 20,
+    color: 'black',
+  },
+  modalContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white', 
+  },
+  modalText: {
+    fontSize: 24,
+    color: 'black',
+  },
+  closeText: {
+    fontSize: 18,
+    color: "black",
+    marginTop: 20,
   },
 });
+
+export default App;
